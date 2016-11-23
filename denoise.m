df922a4aa601cccce8a4d3e0833044f07dd85c7e
yash19970 <yash_19@outlook.com>
2016-11-24 04:52:14 +0530

    Final file for matlab code

--- denoise.m
+++ denoise.m
@@ -1,34 +1,59 @@
-% Matlab Code for Image Noise Reduction using Order Statisticks Filters.
+% Matlab Code for Image Noise Reduction using MedianFilter, and comparing them with desired image.
+PSNR;
+PSNRO = 0;
+PSNRE = 0;
+orimg = imread('sp.jpg');
+distimg = imread('cameraman.jpg');
 
-clc
-clear all 
-prompt = 'Give an Input Image:';
-display prompt
-x = imread('3.jpg');
-%figure();
-%subplot(221);
-%imshow(x); 
-%display 'Given Image';
-%title('Input Image:')
-mask1 = ones(3);
-mask2 = ones(4);
-% convert the rgb image to gray: y rgb2gray(x)
-y = rgb2gray(x);
-figure();
-subplot(1,2,1); imshow(y);title('Input Image')
-subplot(1,2,2); imhist(y);title('Histogram of Input Image')
+for v = 1:2:20
+    b = medfilt2(orimg,[v v]);
+    figure();
+    subplot(1,1,1); imshow(b); title('Filtered Image is for odd mask:');
+end
 
-b = medfilt2(y);
+for v = 2:2:20
+    c = medfilt2(orimg,[v v]);
+    figure();
+    subplot(1,1,1); imshow(c); title('Filtered Image is for even mask:');
+end
+%calculating PSNR.
+orimg = double(orimg);
+distimg = double(distimg);
+[M N] = size(orimg);    
 
-% subplot(1,2,1); imshow(x); and subplot(1,2,2); inhist(x);
-%histogram equalisation
-z = histeq(b);
-%Plots:
-figure();
-subplot(2,2,1); imshow(b); title('Denoised Image By Median Filter')
-subplot(2,2,2); imhist(b); title('Histogram of Image after Median Filter')
-subplot(2,2,3); imshow(z); title('Histogram Equalisation Image')
-subplot(2,2,4); imhist(z); title('Histogram Equalisation')
+%for odd mask values;
+for v =1:2:20
+    orimg = medfilt2(orimg,[v v]);
+    error = orimg - distimg;
+    MSE = sum(sum(error.*error))/(M*N);
+    if(MSE >0)
+        PSNR = 10*log(255*255/MSE)/log(10);
+    end
+    if(PSNR > PSNRO)
+        PSNRO = PSNR;
+        masko = v;
+    end
+end
+disp('PSNR value for Odd mask size is: ');
+disp(PSNRO);
+disp('Best Mask Size is: ');
+disp(masko);
 
 
-
+%for even mask values;
+for v =2:2:20
+    orimg = medfilt2(orimg,[v v]);
+    error = orimg - distimg;
+    MSE = sum(sum(error.*error))/(M*N);
+    if(MSE >0)
+        PSNR = 10*log(255*255/MSE)/log(10);
+    end
+    if(PSNR > PSNRE)
+        PSNRE = PSNR;
+        maske = v;
+    end
+end
+disp('PSNR value for Even mask size is: ');
+disp(PSNRE);
+disp('Best Mask Size is: ');
+disp(maske);
